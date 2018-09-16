eedf35f689dca56497ff37c58cddd5ecd56379ee
Kevin Miao <wkmiao@edu.uwaterloo.ca>
2018-09-15 20:48:18 -0400

    initial commit for rudimentary myo controller

---
+++ myo_controller.py
@@ -0,0 +1,389 @@
+from __future__ import print_function
+import numpy as np
+from os.path import join, dirname
+# import cv2
+import myo as libmyo
+import sys
+import urllib3
+# import cognitive_face as CF
+# from PIL import Image
+# import operator
+import subprocess
+# from os import system
+# from io import BytesIO
+import io
+import requests
+# import matplotlib.pyplot as plt
+# from PIL import Image
+# from matplotlib.patches import Polygon
+# import scipy.misc
+import text
+from collections import Counter, deque
+import time
+import struct
+
+scriptTitle = "Myo Generic Controller"
+
+# def pack(fmt, *args):
+#     return struct.pack('<' + fmt, *args)
+
+# def unpack(fmt, *args):
+#     return struct.unpack('<' + fmt, *args)
+
+# def text(scr, font, txt, pos, clr=(255,255,255)):
+#     scr.blit(font.render(txt, True, clr), pos)
+
+# def multichr(ords):
+#     if sys.version_info[0] >= 3:
+#         return bytes(ords)
+#     else:
+#         return ''.join(map(chr, ords))
+
+# def multiord(b):
+#     if sys.version_info[0] >= 3:
+#         return list(b)
+#     else:
+#         return map(ord, b)
+
+
+class Arm(enum.Enum):
+    UNKNOWN = 0
+    RIGHT = 1
+    LEFT = 2
+
+class XDirection(enum.Enum):
+    UNKNOWN = 0
+    X_TOWARD_WRIST = 1
+    X_TOWARD_ELBOW = 2
+
+class Pose(enum.Enum):
+    REST = 0
+    FIST = 1
+    WAVE_IN = 2
+    WAVE_OUT = 3
+    FINGERS_SPREAD = 4
+    THUMB_TO_PINKY = 5
+    UNKNOWN = 255
+
+
+class Packet(object):
+    def __init__(self, ords):
+        self.typ = ords[0]
+        self.cls = ords[2]
+        self.cmd = ords[3]
+        self.payload = multichr(ords[4:])
+
+    def __repr__(self):
+        return 'Packet(%02X, %02X, %02X, [%s])' % \
+            (self.typ, self.cls, self.cmd,
+             ' '.join('%02X' % b for b in multiord(self.payload)))
+
+
+class MyoRaw(object):
+    '''Implements the Myo-specific communication protocol.'''
+    def __init__(self, tty=None):
+        if tty is None:
+            tty = self.detect_tty()
+        if tty is None:
+            raise ValueError('Myo dongle not found!')
+
+        self.bt = BT(tty)
+        self.conn = None
+        self.emg_handlers = []
+        self.imu_handlers = []
+        self.arm_handlers = []
+        self.pose_handlers = []
+
+    def detect_tty(self):
+        for p in comports():
+            if re.search(r'PID=2458:0*1', p[2]):
+                print('using device:', p[0])
+                return p[0]
+
+        return None
+
+    def run(self, timeout=None):
+        self.bt.recv_packet(timeout)
+
+    def connect(self):
+        ## stop everything from before
+        self.bt.end_scan()
+        self.bt.disconnect(0)
+        self.bt.disconnect(1)
+        self.bt.disconnect(2)
+
+        ## start scanning
+        print('scanning...')
+        self.bt.discover()
+        while True:
+            p = self.bt.recv_packet()
+            print('scan response:', p)
+
+            if p.payload.endswith(b'\x06\x42\x48\x12\x4A\x7F\x2C\x48\x47\xB9\xDE\x04\xA9\x01\x00\x06\xD5'):
+                addr = list(multiord(p.payload[2:8]))
+                break
+        self.bt.end_scan()
+
+        ## connect and wait for status event
+        conn_pkt = self.bt.connect(addr)
+        self.conn = multiord(conn_pkt.payload)[-1]
+        self.bt.wait_event(3, 0)
+
+        ## get firmware version
+        fw = self.read_attr(0x17)
+        _, _, _, _, v0, v1, v2, v3 = unpack('BHBBHHHH', fw.payload)
+        print('firmware version: %d.%d.%d.%d' % (v0, v1, v2, v3))
+
+        self.old = (v0 == 0)
+
+        if self.old:
+            ## don't know what these do; Myo Connect sends them, though we get data
+            ## fine without them
+            self.write_attr(0x19, b'\x01\x02\x00\x00')
+            self.write_attr(0x2f, b'\x01\x00')
+            self.write_attr(0x2c, b'\x01\x00')
+            self.write_attr(0x32, b'\x01\x00')
+            self.write_attr(0x35, b'\x01\x00')
+
+            ## enable EMG data
+            self.write_attr(0x28, b'\x01\x00')
+            ## enable IMU data
+            self.write_attr(0x1d, b'\x01\x00')
+
+            ## Sampling rate of the underlying EMG sensor, capped to 1000. If it's
+            ## less than 1000, emg_hz is correct. If it is greater, the actual
+            ## framerate starts dropping inversely. Also, if this is much less than
+            ## 1000, EMG data becomes slower to respond to changes. In conclusion,
+            ## 1000 is probably a good value.
+            C = 1000
+            emg_hz = 50
+            ## strength of low-pass filtering of EMG data
+            emg_smooth = 100
+
+            imu_hz = 50
+
+            ## send sensor parameters, or we don't get any data
+            self.write_attr(0x19, pack('BBBBHBBBBB', 2, 9, 2, 1, C, emg_smooth, C // emg_hz, imu_hz, 0, 0))
+
+        else:
+            name = self.read_attr(0x03)
+            print('device name: %s' % name.payload)
+
+            ## enable IMU data
+            self.write_attr(0x1d, b'\x01\x00')
+            ## enable on/off arm notifications
+            self.write_attr(0x24, b'\x02\x00')
+
+            # self.write_attr(0x19, b'\x01\x03\x00\x01\x01')
+            self.start_raw()
+
+        ## add data handlers
+        def handle_data(p):
+            if (p.cls, p.cmd) != (4, 5): return
+
+            c, attr, typ = unpack('BHB', p.payload[:4])
+            pay = p.payload[5:]
+
+            if attr == 0x27:
+                vals = unpack('8HB', pay)
+                ## not entirely sure what the last byte is, but it's a bitmask that
+                ## seems to indicate which sensors think they're being moved around or
+                ## something
+                emg = vals[:8]
+                moving = vals[8]
+                self.on_emg(emg, moving)
+            elif attr == 0x1c:
+                vals = unpack('10h', pay)
+                quat = vals[:4]
+                acc = vals[4:7]
+                gyro = vals[7:10]
+                self.on_imu(quat, acc, gyro)
+            elif attr == 0x23:
+                typ, val, xdir = unpack('3B', pay)
+
+                if typ == 1: # on arm
+                    self.on_arm(Arm(val), XDirection(xdir))
+                elif typ == 2: # removed from arm
+                    self.on_arm(Arm.UNKNOWN, XDirection.UNKNOWN)
+                elif typ == 3: # pose
+                    self.on_pose(Pose(val))
+            else:
+                print('data with unknown attr: %02X %s' % (attr, p))
+
+        self.bt.add_handler(handle_data)
+
+
+    def write_attr(self, attr, val):
+        if self.conn is not None:
+            self.bt.write_attr(self.conn, attr, val)
+
+    def read_attr(self, attr):
+        if self.conn is not None:
+            return self.bt.read_attr(self.conn, attr)
+        return None
+
+    def disconnect(self):
+        if self.conn is not None:
+            self.bt.disconnect(self.conn)
+
+    def start_raw(self):
+        '''Sending this sequence for v1.0 firmware seems to enable both raw data and
+        pose notifications.
+        '''
+        self.write_attr(0x28, b'\x01\x00')
+        self.write_attr(0x19, b'\x01\x03\x01\x01\x00')
+        self.write_attr(0x19, b'\x01\x03\x01\x01\x01')
+
+    def mc_start_collection(self):
+        '''Myo Connect sends this sequence (or a reordering) when starting data
+        collection for v1.0 firmware; this enables raw data but disables arm and
+        pose notifications.
+        '''
+        self.write_attr(0x28, b'\x01\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x24, b'\x02\x00')
+        self.write_attr(0x19, b'\x01\x03\x01\x01\x01')
+        self.write_attr(0x28, b'\x01\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x19, b'\x09\x01\x01\x00\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x19, b'\x01\x03\x00\x01\x00')
+        self.write_attr(0x28, b'\x01\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x19, b'\x01\x03\x01\x01\x00')
+
+    def mc_end_collection(self):
+        '''Myo Connect sends this sequence (or a reordering) when ending data collection
+        for v1.0 firmware; this reenables arm and pose notifications, but
+        doesn't disable raw data.
+        '''
+        self.write_attr(0x28, b'\x01\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x24, b'\x02\x00')
+        self.write_attr(0x19, b'\x01\x03\x01\x01\x01')
+        self.write_attr(0x19, b'\x09\x01\x00\x00\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x24, b'\x02\x00')
+        self.write_attr(0x19, b'\x01\x03\x00\x01\x01')
+        self.write_attr(0x28, b'\x01\x00')
+        self.write_attr(0x1d, b'\x01\x00')
+        self.write_attr(0x24, b'\x02\x00')
+        self.write_attr(0x19, b'\x01\x03\x01\x01\x01')
+
+    def vibrate(self, length):
+        if length in xrange(1, 4):
+            ## first byte tells it to vibrate; purpose of second byte is unknown
+            self.write_attr(0x19, pack('3B', 3, 1, length))
+
+
+    def add_emg_handler(self, h):
+        self.emg_handlers.append(h)
+
+    def add_imu_handler(self, h):
+        self.imu_handlers.append(h)
+
+    def add_pose_handler(self, h):
+        self.pose_handlers.append(h)
+
+    def add_arm_handler(self, h):
+        self.arm_handlers.append(h)
+
+
+    def on_emg(self, emg, moving):
+        for h in self.emg_handlers:
+            h(emg, moving)
+
+    def on_imu(self, quat, acc, gyro):
+        for h in self.imu_handlers:
+            h(quat, acc, gyro)
+
+    def on_pose(self, p):
+        for h in self.pose_handlers:
+            h(p)
+
+    def on_arm(self, arm, xdir):
+        for h in self.arm_handlers:
+            h(arm, xdir)
+
+
+
+
+class Myo(MyoRaw):
+	
+
+    def __init__(self, cls, tty=None):
+        MyoRaw.__init__(self, tty)
+        self.cls = cls
+
+        self.history = deque([0] * Myo.HIST_LEN, Myo.HIST_LEN)
+        self.history_cnt = Counter(self.history)
+        self.add_emg_handler(self.emg_handler)
+        self.last_pose = None
+
+        self.pose_handlers = []
+
+    def emg_handler(self, emg, moving):
+        y = self.cls.classify(emg)
+        self.history_cnt[self.history[0]] -= 1
+        self.history_cnt[y] += 1
+        self.history.append(y)
+
+        r, n = self.history_cnt.most_common(1)[0]
+        if self.last_pose is None or (n > self.history_cnt[self.last_pose] + 5 and n > 25 / 2):
+            self.on_raw_pose(r)
+            self.last_pose = r
+
+    def add_raw_pose_handler(self, h):
+        self.pose_handlers.append(h)
+
+    def on_raw_pose(self, pose):
+        for h in self.pose_handlers:
+            h(pose)
+
+
+
+
+
+
+if __name__ == '__main__':
+    import subprocess
+    m = Myo(NNClassifier(), sys.argv[1] if len(sys.argv) >= 2 else None)
+    m.add_raw_pose_handler(print)
+
+    def page(pose):
+        if pose == 5:
+            subprocess.call(['xte', 'key Page_Down'])
+        elif pose == 6:
+            subprocess.call(['xte', 'key Page_Up'])
+
+    m.add_raw_pose_handler(page)
+
+    m.connect()
+
+    while True:
+        m.run()
+
+
+
+def onUnlock():
+	myo.rotSetCenter()
+	myo.unlock("hold")
+
+def getTarget(myo):
+    system('say What do you want to find?')
+    myo.vibrate('short')
+
+def onPoseEdge(pose, edge):
+	if pose == FIST:
+		getTarget(myo)
+	elif pose == WAVE_IN and edge == "on":
+		img_part = "left"
+	elif pose == WAVE_OUT and edge == "on":
+		img_part = "right"
+        myo.unlock("hold")
+        myo.notifyUserAction()
+    	
+
+
+
+			
